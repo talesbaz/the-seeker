08e84c23e14b9e0ea95c393e23881e0afd334acd
talesbaz <talesbaz@gmail.com>
2019-12-17 20:44:48 -0300

    fix topic no leader issue

--- packages/seeker/app.js
+++ packages/seeker/app.js
@@ -1,8 +1,29 @@
+const { Kafka } = require('kafkajs');
+
 const fastify = require('fastify')({
   logger: true
 });
 
-fastify.get('/', async () => ({ 'hello': 'world' }));
+const kafka = new Kafka({
+  clientId: 'seeker',
+  brokers: ['localhost:9092']
+});
+
+const producer = kafka.producer();
+
+fastify.get('/', async () => {
+
+  await producer.connect();
+  await producer.send({
+    topic: 'ISSUE_SONG',
+    messages: [
+      { key: 'key1', value: 'Hello KafkaJS user!' }
+    ]
+  });
+  await producer.disconnect();
+
+  return { 'hello': 'world' };
+});
 
 const start = async () => {
 
